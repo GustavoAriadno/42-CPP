32e9c365d2c0b407d5e4c71b2ec84f68cbfe93c0
GustavoAriadno <gugu.gustavo2011@gmail.com>
2022-02-11 01:12:33 +0100

    ex05 done

---
+++ Module01/ex05/main.cpp
@@ -0,0 +1,18 @@
+#include "Karen.hpp"
+
+int main( void ) {
+	Karen k;
+
+	k.complain("DEBUG");
+	std::cout << std::endl;
+
+	k.complain("INFO");
+	std::cout << std::endl;
+
+	k.complain("WARNING");
+	std::cout << std::endl;
+
+	k.complain("ERROR");
+	std::cout << std::endl;
+	return 0;
+}
