1796222ff19175e729df12ce12df7e4d3a663581
GustavoAriadno <gugu.gustavo2011@gmail.com>
2022-03-26 02:57:39 +0100

    Going

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <string>
 #include <sstream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <climits>
+#include <limits>
+#include <cmath>
+
 
 void	printChar(std::string arg) {
 	std::string	pValue = "Non displayable";
 
-	if (arg.size() > 0) {
+	if (arg.size() > 0 && arg.size() < 4) {
 		std::stringstream	ss;
-		int					c;
+		char				c;
 
 		ss << arg;
 		ss >> c;
-
+		
 		if (20 < c && c < 127) {
 			pValue = "'";
 			pValue += c;
@@ -26,31 +31,48 @@ void	printInt(std::string arg) {
 	std::string	pValue = "impossible";
 
 	if (arg.size() == 1 && arg[0] == '0')
-		std::cout << "int: " << 0 << std::endl;
-	if (arg.size() > 0) {
-		int	n = atoi(arg.c_str());
+		pValue = "0";
+	else if (arg.size() > 0) {
+		long int	n = strtol(arg.c_str(), NULL, 10);
 
-		if (n != 0) {
+		if (n != 0 && n >= INT_MIN && n <= INT_MAX) {
 			std::stringstream	ss;
 			ss << n;
 			ss >> pValue;
 		}
 	}
-
 	std::cout << "int: " << pValue << std::endl;
 }
 
-// void	printFloat(std::string arg) {
-// 	// if ( ) {
-		
-// 	// }
-// 	int		errorMsg;
-// 	float	pValue;
-	
-// 	std::cout
-// 		<< "float: "
-// 		<< (errorMsg ? "impossible" : pValue) << std::endl;
-// }
+//
+//
+//
+//			DAR O MESMO QUE RECEBER
+//			ARG.COMPARE(PVALUE);
+//
+
+void	printDouble(std::string arg) {
+	std::string	pValue = "nan";
+	double		num = atof(arg.c_str()),
+				intPart = 1,
+				fractPart = 1;
+
+	std::stringstream	ss;
+	ss << num;
+	ss >> pValue;
+
+	if (num != atof("inF") && num != atof("-inF"))
+		fractPart = modf(num, &intPart);
+
+	if (num == std::numeric_limits<float>::infinity())
+		pValue = "+" + pValue;
+
+	std::cout
+		<< "double: " << pValue
+		<< (fractPart == 0 ? ".0" : "")
+		<< std::endl;
+}
+
 
 int	main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -59,7 +81,7 @@ int	main(int argc, char *argv[]) {
 		printChar(argv[1]);
 		printInt(argv[1]);
 		// printFloat(argv[1]);
-		// printDouble(argv[1]);
+		printDouble(argv[1]);
 	}
 	return 0;
 }
