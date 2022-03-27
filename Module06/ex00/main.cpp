baf3320d8e859d44b9fb347155a4f384e0ab79de
GustavoAriadno <gugu.gustavo2011@gmail.com>
2022-03-27 20:55:24 +0200

    ex00 done

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -5,29 +5,20 @@
 #include <climits>
 #include <limits>
 #include <cmath>
-
 #include <cctype>
 
 void	printChar(std::string arg) {
 	std::string	pValue = "Non displayable";
 
-	if (arg.size() == 1 && !isdigit(arg[0])) {
-		if (31 < arg[0] && arg[0] < 127) {
-			pValue = "'";
-			pValue += arg[0];
-			pValue += "'";
-		}
+	if (arg.size() == 1 && !isdigit(arg[0]) && isprint(arg[0])) {
+		pValue = "'" + arg + "'";
 	}
 	else if (arg.size() > 1 && !isdigit(arg[0]))
 		pValue = "impossible";
 	else if (arg.size() > 0) {
-		std::stringstream	ss;
-		int					c;
+		char c = static_cast<char>(std::atoi(arg.c_str()));
 
-		ss << arg;
-		ss >> c;
-
-		if (31 < c && c < 127) {
+		if (isprint(c)) {
 			pValue = "'";
 			pValue += c;
 			pValue += "'";
@@ -38,79 +29,70 @@ void	printChar(std::string arg) {
 }
 
 void	printInt(std::string arg) {
-	std::string	pValue = "impossible";
+	std::stringstream	ss;
+	std::string			pValue = "impossible";
+	long int			n = atol(arg.c_str());
 
-	if (arg.size() == 1 && arg[0] == '0')
-		pValue = "0";
-	else if (arg.size() > 0) {
-		long int	n = strtol(arg.c_str(), NULL, 10);
+	if (arg.size() == 1 && !isdigit(static_cast<char>(arg[0])))
+		n = static_cast<int>(arg[0]);
 
-		if (n != 0 && n >= INT_MIN && n <= INT_MAX) {
-			std::stringstream	ss;
-			ss << n;
-			ss >> pValue;
-		}
+	if ((n != 0 && n >= INT_MIN && n <= INT_MAX) ||
+		(n == 0 && arg.size() > 0 && arg[0] == '0')) {
+		ss << n;
+		ss >> pValue;
 	}
+
 	std::cout << "int: " << pValue << std::endl;
 }
 
 void	printFloat(std::string arg) {
-	std::string	pValue = "nan";
-	double		num = atof(arg.c_str()),
-				intPart = 1,
-				fractPart = 1;
-
 	std::stringstream	ss;
+	std::string			pValue;
+	double				num = atof(arg.c_str());
+
+	if (arg.size() == 1 && !isdigit(arg[0]))
+		num = static_cast<float>(arg[0]);
+
 	ss << num;
 	ss >> pValue;
 
-	if (num == 0 && arg[0] != '0') {
-		num = atof("nan");
-		pValue = "nan";
+	if (num == 0 && arg.size() != 1 && arg[0] != '0')
+		pValue = "nanf";
+	else {
+		pValue += ((long double)num == (int)num) ? ".0f" : "f";
+		
+		if (num == std::numeric_limits<float>::infinity())
+			pValue = "+" + pValue;
 	}
 
-	if (pValue == "nan" || num == atof("inF") || num == atof("-inF"))
-		pValue += "f";
-
-	if (pValue != "nanf" && num != atof("inF") && num != atof("-inF")) {
-		fractPart = modf(num, &intPart);
-
-		pValue += (fractPart == 0) ? ".0f" : "f";
-	}
-
-	if (num == std::numeric_limits<float>::infinity())
-		pValue = "+" + pValue;
-
-	std::cout
-		<< "float: " << pValue
-		<< std::endl;
+	std::cout << "float: " << pValue << std::endl;
 }
 
-void	printDouble(std::string arg) {
-	std::string	pValue = "nan";
-	double		num = atof(arg.c_str()),
-				intPart = 1,
-				fractPart = 1;
 
+void	printDouble(std::string arg) {
 	std::stringstream	ss;
+	std::string			pValue;
+	double				num = atof(arg.c_str());
+
+	if (arg.size() == 1 && !isdigit(arg[0]))
+		num = static_cast<double>(arg[0]);
+
 	ss << num;
 	ss >> pValue;
 
-	if (num != atof("inF") && num != atof("-inF"))
-		fractPart = modf(num, &intPart);
-
-	if (num == std::numeric_limits<float>::infinity())
+	if (num == 0 && arg.size() != 1 && arg[0] != '0')
+		pValue = "nan";
+	else if ((long double)num == (int)num)
+		pValue += ".0";
+	else if (num == std::numeric_limits<double>::infinity())
 		pValue = "+" + pValue;
 
-	std::cout
-		<< "double: " << pValue
-		<< (fractPart == 0 ? ".0" : "")
-		<< std::endl;
+	std::cout << "double: " << pValue << std::endl;
 }
 
 int	main(int argc, char *argv[]) {
 	if (argc != 2) {
-		std::cerr << "The program need one argument!" << std::endl;
+		std::cerr << "This program needs one argument!" << std::endl;
 	} else {
 		printChar(argv[1]);
 		printInt(argv[1]);
@@ -119,22 +101,3 @@ int	main(int argc, char *argv[]) {
 	}
 	return 0;
 }
-// ./convert 0
-// char: Non displayable
-// int: 0
-// float: 0.0f
-// double: 0.0
-
-
-// ./convert nan
-// char: impossible
-// int: impossible
-// float: nanf
-// double: nan
-
-
-// ./convert 42.0f
-// char: '*'
-// int: 42
-// float: 42.0f
-// double: 42.0
