a197eca17ce35a634738a0527874d3334f366597
GustavoAriadno <gugu.gustavo2011@gmail.com>
2022-04-01 01:56:49 +0200

    ex01 done

---
+++ Module07/ex01/iter.hpp
@@ -0,0 +1,22 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void	printT( T &v ) { std::cout << v << " "; }
+
+template <typename T>
+void	pow( T &v ) { v *= v; }
+
+template <typename T>
+void	upp( T &v ) { v = toupper(v); }
+
+template <typename T>
+void	iter( T *arr, unsigned int len, void (*f) (T &) ) {
+	for ( unsigned int i = 0 ; i < len ; i++ ) {
+		f(arr[i]);
+	}
+}
+
+#endif
---
+++ Module07/ex01/main.cpp
@@ -0,0 +1,25 @@
+#include "iter.hpp"
+
+int main( void ) {
+	{
+		int		n[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		iter(n, 10, printT);
+		std::cout << std::endl;
+
+		iter(n, 10, pow);
+		std::cout << std::endl;
+
+		iter(n, 10, printT);
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		char	names[5] = "baka";
+
+		iter(names, 5, upp);
+		iter(names, 5, printT);
+		std::cout << std::endl;
+	}
+	return 0;
+}
